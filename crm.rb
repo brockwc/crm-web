63f3b4452ab934296bb691f3b39af8b176e14398
zap-rowsdower <brock.whitbread@gmail.com>
2014-04-04 11:35:57 -0400

    moved contact class to crm.rb file and removed the require

--- crm.rb
+++ crm.rb
@@ -1,8 +1,21 @@
-require './contact.rb'
 require './rolodex.rb'
 require 'sinatra'
-require "sinatra/content_for"
+require 'sinatra/content_for'
+require 'data_mapper'
 
+DataMapper.setup(:default, "sqlite3:database.sqlite3")
+
+class Contact
+  attr_accessor :id, :first_name, :last_name, :email, :note
+
+  def initialize(first_name, last_name, email, note)
+    @first_name = first_name
+    @last_name = last_name
+    @email = email
+    @note = note
+  end
+  
+end
 
 @@rolodex = Rolodex.new
 
